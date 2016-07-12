932c78964e21adeaf312ab62e31a82bb39f96216
s123098 <Shiva.m@ecanarys.com>
2016-07-12 14:34:53 +0530

    Update AppSettings.cs

--- Module3-CD/Source/Setup/HealthClinic.biz/src/MyHealth.Client.Core/AppSettings.cs
+++ Module3-CD/Source/Setup/HealthClinic.biz/src/MyHealth.Client.Core/AppSettings.cs
@@ -25,7 +25,7 @@ namespace MyHealth.Client.Core
         public static readonly System.Uri RedirectUri = new System.Uri("urn:ietf:wg:oauth:2.0:oob");
 
         // HockeyApp AppId
-        public static string HockeyAppID = "HOCKEY_APP_ID";
+        public static string HockeyAppID = "6ea2377b25ed493ca0f31b0cb3011305";
 
         public static string iOSAppGroupIdentifier = "group.healthclinic.client.patients";
 
